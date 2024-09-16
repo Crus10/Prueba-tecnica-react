9dfc6bd2fc48a3947444ceb9984baee9dc5e8b45
Crus10 <juan.cruslol@gmail.com>
2024-09-16 19:46:14 -0300

    Comenzando Prueba-Tecnica
    Realizando prueba tecnica con React

---
+++ Prueba-tecnica/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function App () {
+  return (
+    <>
+      <h1> App de gatitos uwu :3</h1>
+      <button> Obtener Facto de gato :3 </button> <br /> <br />
+      <textarea placeholder='Facto sobre los gatos :3' name='caja' id='cajita' />
+
+    </>
+  )
+}
