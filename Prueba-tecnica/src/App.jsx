1ea878637865e222c197c5c0af1cca51a768aef5
Crus10 <juan.cruslol@gmail.com>
2024-09-25 16:44:29 -0300

    App de gatos
    Prueba tecnica de React

--- Prueba-tecnica/src/App.jsx
+++ Prueba-tecnica/src/App.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useFetch } from './useFetch'
 
 export function App () {
+  const { data, loading, error } = useFetch('https://catfact.ninja/fact')
+
+  const firstWord = data?.fact.split(' ')[0]
+
+  const [dataImage, setDataImage] = useState(null)
+  const [loadingImage, setLoadingImage] = useState(true)
+  const [errorImage, setErrorImage] = useState(null)
+
+  useEffect(() => {
+    if (firstWord) {
+      setLoadingImage(true)
+      fetch(`https://cataas.com/cat/says/${firstWord}`)
+        .then((dataImage) => setDataImage(dataImage))
+        .catch((errorImage) => setErrorImage(errorImage))
+        .finally(() => setLoadingImage(false))
+    }
+  }, [firstWord])
+
   return (
     <>
-      <h1> App de gatitos uwu :3</h1>
-      <button> Obtener Facto de gato :3 </button> <br /> <br />
-      <textarea placeholder='Facto sobre los gatos :3' name='caja' id='cajita' />
+      <h1> 😼 App de gatitos uwu 😼</h1> <br />
 
+      <button onClick={() => window.location.reload()}> NUEVO FACT DE GATOS  </button>
+      <h2> {data?.fact}
+        {error && <div>Error:{error} </div>}
+        {loading && <div>Loading...</div>}
+      </h2>
+      {firstWord && <h3>La primera palabra es : {firstWord}</h3>}
+
+      {loadingImage && <div>Loading...</div>}
+      {errorImage && <div>Error: {errorImage.message}</div>}
+      {dataImage && (
+
+        <div>
+          <img src={dataImage.url} alt='Imagen de gatito' />
+        </div>
+      )}
     </>
   )
 }
